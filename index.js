997e8ebd2869935f234f66c31b77cfcfba386e48
Chad Smith <chad.smith@vanderbilt.edu>
2022-07-11 23:32:02 -0400

    handled two out of seven menu selections

--- index.js
+++ index.js
@@ -16,31 +16,167 @@ const displayMenu = () => {
                     console.log('Please choose an option!');
                 }
             }
-    
         }
     ])
-
     .then(menu => {
         if (menu.menuSelection === 'View All Departments') {
             const sql = `SELECT * FROM department`
+            console.table(  
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    return rows
+                })
+            )
+        }
+        else if (menu.menuSelection === 'View All Roles') {
+            const sql = `SELECT * FROM role`
             
-            const json = db.query(sql, (err, rows) => {
-                            if (err) {
-                                res.status(500).json({ error: err.message });
-                                return;
-                            }
-                            res.json({
-                                message: 'success',
-                                data: rows
-                            });
-                        })
+            console.table(  
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    res.json({
+                        message: 'success',
+                        data: rows
+                    });
+                })
+            )
+        }
 
-            console.table(json)
+        else if (menu.menuSelection === 'View All Employees') {
+            const sql = `SELECT * FROM employee`
+            
+            console.table(  
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    res.json({
+                        message: 'success',
+                        data: rows
+                    });
+                })
+            )
+        }
+
+        else if (menu.menuSelection === 'Add a Department') {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'departmentName',
+                    message: 'What is the name of the department?',
+                    validate: menu => {
+                        if (menu) {
+                            return true;
+                        } else {
+                            console.log('Please choose an option!');
+                        }
+                    }
+                }
+            ])
+            .then( input => {
+                const sql = `INSERT INTO department (name)
+                    VALUES
+                    ('${input.departmentName}');`
+                db.query(sql, (err, rows) => {
+                    return rows
+                })
+                console.log(`Added ${input.departmentName} to the database.`)
+            })
+        }
+
+        else if (menu.menuSelection === 'Add a Role') {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'roleName',
+                    message: 'What is the name of the role?',
+                    validate: menu => {
+                        if (menu) {
+                            return true;
+                        } else {
+                            console.log('Please choose an option!');
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'roleSalary',
+                    message: 'What is the salary of the role?',
+                    validate: menu => {
+                        if (menu) {
+                            return true;
+                        } else {
+                            console.log('Please choose an option!');
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'roleDepartment',
+                    message: 'Which department does the role belong to?',
+                    validate: menu => {
+                        if (menu) {
+                            return true;
+                        } else {
+                            console.log('Please choose an option!');
+                        }
+                    }
+                }
+            ])
+            .then( input => {
+                const sql = `INSERT INTO role (title, salary)
+                    VALUES
+                    ('${input.roleName}'),
+                    ('${input.roleSalary}),`
+                db.query(sql, (err, rows) => {
+                    return rows
+                })
+                console.log(`Added ${input.roleName} to the database.`)
+            })
+        }
+
+        else if (menu.menuSelection === 'Add an Employee') {
+            const sql = ``
+            
+            console.table(  
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    res.json({
+                        message: 'success',
+                        data: rows
+                    });
+                })
+            )
+        }
+
+        else if (menu.menuSelection === 'Update an Employee Role') {
+            const sql = `SELECT * FROM department`
+            
+            console.table(  
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    res.json({
+                        message: 'success',
+                        data: rows
+                    });
+                })
+            )
         }
     })
 };
 
-
-const viewDepartments = 
 
 displayMenu();
